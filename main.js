8d95c8a16e2809fdb89c75b59637fec6cc8d4c07
liliethisamaracanalesrios <liliethisamaracanalesrios@gmail.com>
2026-06-24 23:26:20 -0600

    main actualizado

--- main.js
+++ main.js
@@ -27,6 +27,32 @@ async function init() {
         },
     ]);
 
-    console.log (chalk.green('Opción seleccionada: ' + setup.opcion));
+    console.log (chalk.bgGray.green('Opción seleccionada: ' + setup.opcion));
     return setup.opcion;
 }
+
+async function MainMenu(opcion) {
+    if (opcion === '1') {
+        const estudianteController = new EstudianteController(opcion);
+        await estudianteController.init();
+    }   else if (opcion === '2') {
+        const libroController = new LibroController(opcion);
+        await libroController.init();
+    }   else if (opcion === '3') {
+        console.log(chalk.bgGray.yellow('Hasta luego'));
+    }   else {
+        console.log(
+            chalk.bgGray.red(
+            'Opción no válida'
+
+        )
+    );
+} 
+}
+
+let opcion; 
+do{
+    console.clear();
+    opcion = await init();
+    await MainMenu(opcion);
+}   while (opcion !== '3');
